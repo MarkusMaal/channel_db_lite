fe8e3af58833526872a6fdb89e1d2736111d167d
Markus Maal <markus.maal@gmail.com>
2024-02-21 14:30:18 +0200

    Channel&category filters in videos and advanced search functionality

--- controllers/VideoController.php
+++ controllers/VideoController.php
@@ -8,6 +8,7 @@ use app\models\Video;
 
 class VideoController extends Controller
 {
+    // all videos
     public function actionIndex()
     {
         $query = Video::find();
@@ -20,16 +21,74 @@ class VideoController extends Controller
         ->offset($pagination->offset)
         ->limit($pagination->limit)
         ->all();
+
+        $categories = Video::find()->orderBy('Category')->select('Category')->distinct()->all();
+        $channels = Video::find()->orderBy('Kanal')->select('Kanal')->distinct()->all();
         return $this->render('index', [
             'videos'     => $videos,
+            'channels'   => $channels,
             'pagination' => $pagination,
+            'categories' => $categories,
         ]);
     }
 
+    // single entry
     public function actionView($id) {
         $video = Video::findOne($id);
         return $this->render('view', [
             'video' => $video,
+        ]);
+    }
+
+    // simple search
+    public function actionSearch($q) {
+        $query = Video::find()->where(["like", "CONCAT(Video, Kanal, Kirjeldus, URL, Kuupäev, Filename, Category, Tags, OdyseeURL)", $q]);
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $query->count(),
+        ]);
+
+        $videos = $query->orderBy('Kuupäev '.($_GET["ord"]??'DESC'))
+        ->offset($pagination->offset)
+        ->limit($pagination->limit)
+        ->all();
+        $categories = Video::find()->orderBy('Category')->select('Category')->distinct()->all();
+        $channels = Video::find()->orderBy('Kanal')->select('Kanal')->distinct()->all();
+        return $this->render('index', [
+            'videos'     => $videos,
+            'pagination' => $pagination,
+            'channels'   => $channels,
+            'categories' => $categories,
+        ]);
+    }
+
+    // advanced search (url: adv-search)
+    public function actionAdvSearch($q = "", $ch = "", $del = "", $sub = "", $pub = "", $live = "", $hd = "", $cat = "") {
+        $query = Video::find()
+        ->where(["like", "CONCAT(Video, Kanal, Kirjeldus, URL, Kuupäev, Filename, Category, Tags, OdyseeURL)", $q])
+        ->andWhere(["like", "Kanal", $ch])
+        ->andWhere(["like", "Kustutatud", $del])
+        ->andWhere(["like", "Subtiitrid", $sub])
+        ->andWhere(["like", "Avalik", $pub])
+        ->andWhere(["like", "Ülekanne", $live])
+        ->andWhere(["like", "HD", $hd])
+        ->andWhere(["like", "Category", $cat]);
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $query->count(),
+        ]);
+
+        $videos = $query->orderBy('Kuupäev '.($_GET["ord"]??'DESC'))
+        ->offset($pagination->offset)
+        ->limit($pagination->limit)
+        ->all();
+        $categories = Video::find()->orderBy('Category')->select('Category')->distinct()->all();
+        $channels = Video::find()->orderBy('Kanal')->select('Kanal')->distinct()->all();
+        return $this->render('index', [
+            'videos'     => $videos,
+            'pagination' => $pagination,
+            'channels'   => $channels,
+            'categories' => $categories,
         ]);
     }
 
--- views/video/index.php
+++ views/video/index.php
@@ -6,9 +6,75 @@ use yii\widgets\LinkPager;
 
 $this->title = 'Videod' . (isset($_GET["page"])?" - Leht ".$_GET["page"]:"");
 $baseurl = str_replace($_SERVER["DOCUMENT_ROOT"], "", Yii::$app->basePath);
+
+function ClearFilter($preurl, $name) {
+    $name = str_replace("+", "%2B", $name);
+    if (!isset($_GET[$name])) {return $preurl;}
+    return "<a href='".str_replace("&".$name."=".$_GET[$name], "", str_replace("?".$name."=".$_GET[$name], "", $preurl))."'>";
+}
+
+function AddFilter($preurl, $name, $value) {
+    $preurl = str_replace("'>", "", str_replace("<a href='", "", ClearFilter($preurl, $name)));
+    if (str_contains($preurl, "?")) {
+        return $preurl."&$name=$value";
+    } else {
+        return $preurl."?$name=$value";
+    }
+}
 ?>
 <div class="mx-auto text-center">
-<?= "<a class=\"btn btn-secondary m-2 text-center\" href=\"?ord=" . (!empty($_GET["ord"])?(($_GET["ord"] == "DESC")?"ASC\">Õigetpidi järjestus":"DESC\">Tagurpidi järjestus"):"ASC\">Õigetpidi järjestus") . "</a>" ?>
+<?php
+$url = $_SERVER["REQUEST_URI"];
+$params = "?".substr($url, strrpos($url, '?') + 1);
+$pref = (str_contains($params, "=")?"&":"?");
+$preurl = str_replace("&ord=".($_GET["ord"]??"DESC"), "", $url);
+echo "<a class=\"btn btn-secondary m-2 text-center\" href=\"$preurl{$pref}ord=" . (!empty($_GET["ord"])?(($_GET["ord"] == "DESC")?"ASC\">Õigetpidi järjestus":"DESC\">Tagurpidi järjestus"):"ASC\">Õigetpidi järjestus") . "</a>";
+if (!isset($_GET["cat"])) {
+?>
+<div class="dropdown d-inline">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="categorySelectButton" data-bs-toggle="dropdown" aria-expanded="false">
+        Kategooria
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="categorySelectButton">
+
+        <?php foreach ($categories as $category) { ?>
+            <li><a class="dropdown-item" href="<?= AddFilter(str_replace("video/index", "video/adv-search", $preurl), "cat", $category->Category); ?>"><?= $category->Category ?></a></li>
+        <?php } ?>
+    </ul>
+</div>
+<?php
+}
+if (!isset($_GET["ch"])) {
+?>
+<div class="dropdown d-inline ms-2">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="categorySelectButton" data-bs-toggle="dropdown" aria-expanded="false">
+        Kanal
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="categorySelectButton">
+
+        <?php foreach ($channels as $channel) { ?>
+            <li><a class="dropdown-item" href="<?= AddFilter(str_replace("video/index", "video/adv-search", $preurl), "ch", str_replace("+", "%2B", $channel->Kanal)); ?>"><?= $channel->Kanal ?></a></li>
+        <?php } ?>
+    </ul>
+</div>
+<?php
+}
+$o = "<p>Filtrid:&nbsp;";
+$hasFilters = false;
+if (isset($_GET["search"])) { $o .= ClearFilter($preurl, "search")."<span class='badge bg-secondary mx-2'>".$_GET["search"]."</span></a>"; $hasFilters = true; }
+if (isset($_GET["ch"])) { $o .= ClearFilter($preurl, "ch")."<span class='badge bg-secondary mx-2'>Kanal: ".$_GET["ch"]."</span></a>"; $hasFilters = true; }
+if (isset($_GET["del"])) { $o .= ClearFilter($preurl, "del")."<span class='badge bg-secondary mx-2'> ".($_GET["del"]=="1"?"Kustutatud":"Ei ole kustutatud")."</span></a>"; $hasFilters = true; }
+if (isset($_GET["live"])) { $o .= ClearFilter($preurl, "live")."<span class='badge bg-secondary mx-2'> ".($_GET["live"]=="1"?"Otseülekanne":"Ei ole otseülekanne")."</span></a>"; $hasFilters = true; }
+if (isset($_GET["hd"])) { $o .= ClearFilter($preurl, "hd")."<span class='badge bg-secondary mx-2'> ".($_GET["hd"]=="1"?"Kõrge kvaliteet":"Madal kvaliteet")."</span></a>"; $hasFilters = true; }
+if (isset($_GET["sub"])) { $o .= ClearFilter($preurl, "sub")."<span class='badge bg-secondary mx-2'> ".($_GET["hd"]=="1"?"Sisaldab subtiitreid":"Subtiitriteta")."</span></a>"; $hasFilters = true; }
+if (isset($_GET["pub"])) { $o .= ClearFilter($preurl, "pub")."<span class='badge bg-secondary mx-2'> ".($_GET["hd"]=="1"?"Avalik":"Pole avalik")."</span></a>"; $hasFilters = true; }
+if (isset($_GET["cat"])) { $o .= ClearFilter($preurl, "cat")."<span class='badge bg-secondary mx-2'> Kategooria: ".$_GET["cat"]."</span></a>"; $hasFilters = true; }
+if (isset($_GET["q"])) { $o .= ClearFilter($preurl, "q")."<span class='badge bg-secondary mx-2'>Märksõna(d): ".$_GET["q"]."</span></a>"; $hasFilters = true; }
+$o .= "</p>";
+if ($hasFilters) {
+    echo $o;
+}
+?>
 </div>
 <?= LinkPager::widget([
     'pagination' => $pagination,
