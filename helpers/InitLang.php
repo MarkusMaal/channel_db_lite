e6f40f1f38fd1179800867fe8d7aa6db6694dc47
Markus Maal <markus.maal@gmail.com>
2024-02-25 18:23:24 +0200

    Fix lang cookie problems

--- helpers/InitLang.php
+++ helpers/InitLang.php
@@ -3,10 +3,6 @@
 $cookies = Yii::$app->request->cookies;
 if (!isset($_COOKIE["lang_"]) && isset($_COOKIE["lang"])) {
     $cookies = Yii::$app->response->cookies;
-    $cookies->add(new \Yii\web\Cookie([
-        "name"=> "lang_",
-        "value"=> $_COOKIE["lang"],
-    ]));
     Yii::$app->language = $_GET["lang"];
 } else if ($cookies->getValue("lang_", "et-EE") != "et-EE") {
     Yii::$app->language = "en-US";
