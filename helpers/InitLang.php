3cf1ee0cd92e09badbf522bce24695d5c8c22e8b
Markus Maal <markus.maal@gmail.com>
2024-02-25 18:28:35 +0200

    Remove site/app language sync

--- helpers/InitLang.php
+++ helpers/InitLang.php
@@ -1,9 +1,6 @@
 <?php
 // initialize multi-language support
 $cookies = Yii::$app->request->cookies;
-if (!isset($_COOKIE["lang_"]) && isset($_COOKIE["lang"])) {
-    $cookies = Yii::$app->response->cookies;
-    Yii::$app->language = $_GET["lang"];
-} else if ($cookies->getValue("lang_", "et-EE") != "et-EE") {
+if ($cookies->getValue("lang_", "et-EE") != "et-EE") {
     Yii::$app->language = "en-US";
 }
