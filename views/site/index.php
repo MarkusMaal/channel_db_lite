4ea6a3466e73a14856d6a5695642a8897486651f
Markus Maal <markus.maal@gmail.com>
2024-02-29 18:57:30 +0200

    Fancier jumbotron on site index

--- views/site/index.php
+++ views/site/index.php
@@ -7,15 +7,24 @@ require_once(Yii::getAlias("@app/helpers/InitLang.php"));
 $this->title = Yii::t('site', 'Kanali andmebaas');
 ?>
 <div class="site-index">
-
-    <div class="jumbotron bg-transparent mt-5 mb-5">
+    <div class="jumbotron bg-light p-5 mt-5 mb-5 d-flex">
+    <img class="me-5" src="<?= Url::to("@web/channel_db_lite.svg", true) ?>">
+        <div>
         <h1 class="display-5"><?= Yii::t('app', 'Kanali andmebaas Lite') ?></h1>
-        <?php isset($_GET["q"])?Yii::$app->session->setFlash('blurple', Yii::t('site', 'Otsimiseks palun sisenege videote, ideede või galerii lehele')):"" ?>
-        <p class="lead"><?= Yii::t('site', 'Uue koodiga versioon kanali andmebaasist, mis on kirjutatud {0}. Kanali andmebaas Lite ei sisalda üksikuid funktsioone, mis olid varasemas versioonis, sealhulgas...', ["<a class='link-blurple' href='https://yiiframework.com' target='_blank'>".Yii::t("site", "Yii raamistikus")."</a>"]) ?></p>
-        <ul>
-            <li><?= Yii::t('site', 'Raporti salvestamine CSV vormingusse'); ?></li>
-            <li><?= Yii::t('site', 'Pärandrežiim'); ?></li>
-        </ul>
+                <?php isset($_GET["q"])?Yii::$app->session->setFlash('blurple', Yii::t('site', 'Otsimiseks palun sisenege videote, ideede või galerii lehele')):"" ?>
+                <p class="lead"><?= Yii::t('site', 'Uue koodiga versioon kanali andmebaasist, mis on kirjutatud {0}. Kanali andmebaas Lite ei sisalda üksikuid funktsioone, mis olid varasemas versioonis, sealhulgas...', ["<a class='link-blurple' href='https://yiiframework.com' target='_blank'>".Yii::t("site", "Yii raamistikus")."</a>"]) ?></p>
+                <ul>
+                    <li><?= Yii::t('site', 'Raporti salvestamine CSV vormingusse'); ?></li>
+                    <li><?= Yii::t('site', 'Pärandrežiim'); ?></li>
+                </ul>
+        <a class="btn btn-lg btn-blurple me-2" href="<?= Url::to(["/video/adv-search"]) ?>"><?= Yii::t("app", "Videod") ?></a>
+        <a class="btn btn-lg btn-orangellow me-2" href="<?= Url::to(["/ideas/adv-search"]) ?>"><?= Yii::t("app", "Ideed") ?></a>
+        <a class="btn btn-lg btn-skyblue me-2" href="<?= Url::to(["/gallery/index"]) ?>"><?= Yii::t("app", "Galerii") ?></a>
+        <a class="btn btn-lg btn-primary me-2" href="<?= Url::to("/channel_db?ord=id&dir=DESC", true) ?>"><?= Yii::t("app", "Täisversioon") ?></a>
+        <a class="btn btn-lg btn-secondary" href="<?= Url::to("https://markustegelane.eu/channel_db_old") ?>"><?= Yii::t("app", "Vana kujundus") ?></a>
+        </div>
+    </div>
+    <div>
         <h3 style="font-weight: normal;"><?= Yii::t('site', 'Kuid milleks luua veel üks uus version kanali andmebaasist?'); ?></h3>
         <p><?= Yii::t('site', 'Põhjuseid on mitmeid, kuid siin on mõned olulisemad:') ?></p>
         <ul>
@@ -29,10 +38,5 @@ $this->title = Yii::t('site', 'Kanali andmebaas');
             <li><?= Yii::t('site', 'Vana kanali andmebaas on täis haavatavusi ja veidraid bugisid, mis on vaja likvideerida'); ?></li>
             <li><?= Yii::t('site', 'Yii2 sisaldab paremaid silumistööriistu vigade leidmiseks'); ?></li>
         </ul>
-        <a class="btn btn-lg btn-blurple me-2" href="<?= Url::to(["/video/adv-search"]) ?>"><?= Yii::t("app", "Videod") ?></a>
-        <a class="btn btn-lg btn-orangellow me-2" href="<?= Url::to(["/ideas/adv-search"]) ?>"><?= Yii::t("app", "Ideed") ?></a>
-        <a class="btn btn-lg btn-skyblue me-2" href="<?= Url::to(["/gallery/index"]) ?>"><?= Yii::t("app", "Galerii") ?></a>
-        <a class="btn btn-lg btn-primary me-2" href="<?= Url::to("/channel_db?ord=id&dir=DESC", true) ?>"><?= Yii::t("app", "Täisversioon") ?></a>
-        <a class="btn btn-lg btn-secondary" href="<?= Url::to("https://markustegelane.eu/channel_db_old") ?>"><?= Yii::t("app", "Vana kujundus") ?></a>
     </div>
 </div>
